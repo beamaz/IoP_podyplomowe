e9c203a52e2ccfbe52ded1c371ee549c902f1f8b
beamaz <mazgaj.beata@gmail.com>
2022-02-16 21:15:37 +0100

    gitignore-with-code

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserAccount firstUser = new UserAccount();
+        firstUser.createUser();
+    }
+}
