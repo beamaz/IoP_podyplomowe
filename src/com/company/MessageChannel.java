1a8927fc60d294562b36c240f825b6bf6c4744dd
beamaz <mazgaj.beata@gmail.com>
2022-02-16 21:10:25 +0100

    cleaning-up

--- src/com/company/MessageChannel.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public enum MessageChannel {
-
-    Email,
-    Notification
-}
--- src/com/company/UserType.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public enum UserType {
-
-    Client,
-    Receptionist,
-    Trainer
-}
